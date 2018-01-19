dbb8287a53d0ada9032cb2a4e754cc1519827576
Kevin Coyner <rustybear@gmail.com>
2018-01-18 20:51:39 -0500

    Update to make completely POSIX compliant.

--- migrate.sh
+++ migrate.sh
@@ -6,7 +6,7 @@
 
 set -e
 
-if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
   cat << EOM
 
   Usage:   $0  old_repo_name.git  new_repo_name
@@ -39,47 +39,47 @@ fi
 
 old_repo_name=$1
 new_repo_name=$2
-dir_name=`basename $(pwd)`
+dir_name=$(basename "$(pwd)")
 
-if [[ "$old_repo_name" == "" ]]; then
+if [ "$old_repo_name" = "" ]; then
   echo "You did not specify an old repo name. Try again."
   exit 1
 fi
 
-if [[ "$new_repo_name" == "" ]]; then
-  echo "New repo name ?\n(or hit enter to use $dir_name as a new repo name)"
+if [ "$new_repo_name" = "" ]; then
+  printf "%s\n %s\n" "New repo name ?" "(or hit enter to use $dir_name as a new repo name)"
   read new_repo_name
 fi
 
-if [[ "$new_repo_name" == "" ]]; then
+if [ "$new_repo_name" = "" ]; then
   new_repo_name=$dir_name
 fi
 
-username=`git config github.user`
-if [[ "$username" = "" ]]; then
+username=$(git config github.user)
+if [ "$username" = "" ]; then
   echo "Could not find username, run 'git config --global github.user <username>'"
   exit 1
 fi
 
-git clone --bare https://github.com/$username/$old_repo_name || (echo "fatal: Could\
+git clone --bare https://github.com/"$username"/"$old_repo_name" || (echo "fatal: Could\
  not find your old repo on github or you do not have access rights to it." && exit 1)
 
-if [[ -f "/usr/bin/curl" ]]; then
-  /usr/bin/curl -s -u $username https://api.github.com/user/repos \
+if [ -f "/usr/bin/curl" ]; then
+  /usr/bin/curl -s -u "$username" https://api.github.com/user/repos \
   -d "{\"name\":\"$new_repo_name\"}" || \
   (echo "fatal: Could not create a new repo on github." && exit 1)
 else
   echo "Error:  You must have 'curl' installed on your system."
-  rm -rf $old_repo_name || (echo "fatal: Could not remove the old repo." && exit 1)
+  rm -rf "$old_repo_name" || (echo "fatal: Could not remove the old repo." && exit 1)
   exit 1
 fi
 
-cd $old_repo_name || (echo "fatal: Could not change directories." && exit 1)
+cd "$old_repo_name" || (echo "fatal: Could not change directories." && exit 1)
 
-git push --mirror https://github.com/$username/$new_repo_name || \
+git push --mirror https://github.com/"$username"/"$new_repo_name" || \
   (echo "fatal: Could not populate your new repo with data." && exit 1)
 
-(cd .. && rm -rf $old_repo_name) || (echo "fatal: Could not remove the old repo." && exit 1)
+(cd .. && rm -rf "$old_repo_name") || (echo "fatal: Could not remove the old repo." && exit 1)
 
 cat << EOM
 
